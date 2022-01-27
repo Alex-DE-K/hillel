4f24941968d6254248a762ff261416f0b79708d8
Alexey Kelin <alexey.kelin@gmail.com>
2022-01-27 10:31:01 +0200

    HW02 task 1

--- HW02/index.js
+++ HW02/index.js
@@ -2,12 +2,17 @@ const userLogin = prompt ("Pls enter Login");
 
 if (userLogin === 'admin') {
     userPassword = prompt ("Pls enter the password");
-    
 
-}
-else if (userLogin === null) {
+        if (userPassword === 'BlackMaster') {
+            alert ("wellcome");
+        } else if (userPassword === null){
+            alert ("exit");
+        } else {
+            alert ("wrong password");
+        };   
+
+} else if (userLogin === null) {
     alert ("exit");
-}
-else {
+} else {
     alert ("user unknown");
-}
+};

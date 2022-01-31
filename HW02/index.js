43233512201097b37d2cd3da52173de8b0bc2f80
Alexey Kelin <alexey.kelin@gmail.com>
2022-01-31 10:56:14 +0200

    alternative if_else

--- HW02/index.js
+++ HW02/index.js
@@ -1,18 +1,31 @@
 const userLogin = prompt ("Pls enter Login");
+let userPassword = 0;
 
 if (userLogin === 'admin') {
     userPassword = prompt ("Pls enter the password");
 
-        if (userPassword === 'BlackMaster') {
-            alert ("wellcome");
-        } else if (userPassword === null){
-            alert ("exit");
-        } else {
-            alert ("wrong password");
-        };   
+        // if (userPassword === 'BlackMaster') {
+        //     alert ("wellcome");
+        // } else if (userPassword === null){
+        //     alert ("exit");
+        // } else {
+        //     alert ("wrong password");
+        // }
+
+        switch (userPassword) {
+            case 'BlackMaster':
+                alert ("wellcome");
+                break;
+            case null:
+                alert ("exit");
+                break;
+            default: 
+                alert ("wrong password");
+                break;
+        }
 
 } else if (userLogin === null) {
     alert ("exit");
 } else {
     alert ("user unknown");
-};
+}

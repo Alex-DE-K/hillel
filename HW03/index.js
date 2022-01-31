02710b2cf576f2fb2859e5e3e832c9a89cd72c6b
Alexey Kelin <alexey.kelin@gmail.com>
2022-01-31 11:50:23 +0200

    task3

--- HW03/index.js
+++ HW03/index.js
@@ -3,9 +3,14 @@
 // 1.3 Посчитать сумму чётных чисел
 
 let sum = 0;
+let evenNumbersSum = 0;
 
 for (i = 1; i < 150; i++) {
     sum += i;
+    if (i % 2 === 0) {
+        evenNumbersSum +=i;
+    }
 }
 
 console.log(sum);
+console.log(evenNumbersSum);
